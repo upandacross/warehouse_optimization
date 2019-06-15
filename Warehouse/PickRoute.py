d41098d53dd2621b532f8e745cf902697e7d3b5c
upandacross <1205122+upandacross@users.noreply.github.com>
2019-06-15 16:49:10 -0500

    lru + vectorize to bin_to_bin_distance, wh is classvar, fix calc_route

--- Warehouse/PickRoute.py
+++ Warehouse/PickRoute.py
@@ -3,12 +3,14 @@ Created on May 28, 2019
 
 @author: bren
 '''
+from builtins import isinstance
 from collections import OrderedDict
+from functools import lru_cache
+import numpy as np
 from Warehouse.Bin import Bin
 from Warehouse.Inventory import Inventory
 from Warehouse.Order import Order
 from Warehouse.Warehouse import Warehouse
-from builtins import isinstance
 
 class PickRoute:
     '''
@@ -16,7 +18,30 @@ class PickRoute:
     '''
     
     __inventory = Inventory()
+    wh = None
 
+
+    @lru_cache(maxsize=5000)
+    def __bin_to_bin_distance(self, from_bin, to_bin):
+        '''Manhattan distance, not Euclidian
+        call with two Bin class instances
+    '''
+        assert isinstance(from_bin, Bin) and isinstance(to_bin, Bin), 'both arguments must be instance of Bin class'
+        rack_diff = to_bin.rack_no - from_bin.rack_no
+        if (rack_diff == 0 and from_bin.bin_side == to_bin.bin_side) or\
+           (rack_diff == 1 and (from_bin.bin_side, to_bin.bin_side) == ('b', 'a')):
+            lat_long_distance = np.abs(from_bin.lat_long - to_bin.lat_long).sum()
+        else:
+            r = PickRoute.wh.racks[to_bin.rack_no - 1]
+            nc_to = r.nearest_cap(to_bin)
+            r = PickRoute.wh.racks[from_bin.rack_no - 1]
+            nc_from = r.nearest_cap(from_bin)
+            
+            nc_bin = nc_to if nc_to < nc_from else nc_from
+            f_2_t = np.hstack([from_bin.lat_long, nc_bin.lat_long, to_bin.lat_long]).reshape(3, 2)
+            lat_long_distance = np.abs(np.diff(f_2_t, axis=0)).sum()
+        return lat_long_distance
+    
 
     def __init__(self, wh, order):
         '''
@@ -41,88 +66,62 @@ class PickRoute:
                     self.__pick_bins.add(Bin.get_bin_by_location(item_location))
 
         self.__wh = wh
+        PickRoute.wh = wh
         self.__route = OrderedDict()
         self.__route_distance = 0
         
         self.__calc_route()
         
 
+    def bin_to_bin_distance(self, from_bin, to_bin):
+        return self.__bin_to_bin_distance(from_bin, to_bin)
+
 
     def __calc_route(self):
         pick_items = self.__pick_items.copy()
-        pick_bins = self.__pick_bins.copy()
+        pick_bins = sorted(self.__pick_bins.copy())
         
         # first one starts from dock, ends on nearest to first rack, side a, first bin
-        to_bin = min(self.__pick_bins)
+        to_bin = pick_bins[0]
         from_bin = self.__wh.dock
-        self.__route_distance += self.bin_to_bin_distance(from_bin, to_bin)
+        self.__route_distance += self.bin_to_bin_distance(*sorted((from_bin, to_bin)))
         self.__route[(from_bin.location, to_bin.location)] = self.__route_distance
         pick_items.remove(to_bin.item)
         pick_bins.remove(to_bin)
+        from_bin = to_bin
         # pick closest to 
-        while len(pick_items) > 0:
-            from_bin = to_bin
+        while len(pick_items) > 0 and len(pick_bins) > 0:
+            to_bin = None
             min_bin_dist = 1e10
-            for check_bin in sorted(pick_bins):
+            min_item = None
+            for check_bin in pick_bins:
                 if check_bin.item not in pick_items:
+                    pick_bins.remove(check_bin)
+                    check_bin = None
                     continue # already picked this item
-                d = self.bin_to_bin_distance(from_bin, check_bin)
-                if d < min_bin_dist:
-                    min_bin_dist = d 
-                    to_bin = check_bin
-            if check_bin is not None:
+                else:
+                    d = self.bin_to_bin_distance(*sorted((from_bin, check_bin)))
+                    if d < min_bin_dist:
+                        min_item = check_bin.item
+                        min_bin_dist = d 
+                        to_bin = check_bin
+            if to_bin is not None:
                 self.__route_distance += min_bin_dist
                 self.__route[(from_bin.location, to_bin.location)] = min_bin_dist
-                try:
+                if min_item is not None:
                     pick_items.remove(to_bin.item)
-                except Exception as e:
-                    print(e)
                 pick_bins.remove(to_bin)
+                from_bin = to_bin
             pass # for debugging
         
         # last step is to dock
         from_bin = to_bin
         to_bin = self.__wh.dock
-        self.__route_distance += self.bin_to_bin_distance(from_bin, to_bin)
+        self.__route_distance += self.bin_to_bin_distance(*sorted((from_bin, to_bin)))
         self.__route[(from_bin.location, to_bin.location)] = self.__route_distance
         return None
         
-        
-    def bin_to_bin_distance(self, from_bin, to_bin):
-        '''Manhattan distance, not Euclidian
-        call with two Bin class instances
-'''
-        assert isinstance(from_bin, Bin) and isinstance(to_bin, Bin), 'both arguments must be instance of Bin class'
-        rack_diff = to_bin.rack_no - from_bin.rack_no
-        if (rack_diff == 0 and from_bin.bin_side == to_bin.bin_side) or\
-           (rack_diff == 1 and (from_bin.bin_side, to_bin.bin_side) == ('b', 'a')):
-            lat_distance = abs(from_bin.lat - to_bin.lat)
-            #from_long = from_bin.long if from_bin.bin_side == 'b' else from_bin.long - 1
-            #to_long = to_bin.long if to_bin.bin_side == 'b' else to_bin.long - 1
-            #lng_distance = abs(from_long - to_long)
-            lng_distance = abs(from_bin.long - to_bin.long)
-        else:
-            r = self.wh.racks[to_bin.rack_no - 1]
-            nc_to = r.nearest_cap(to_bin)
-            r = self.wh.racks[from_bin.rack_no - 1]
-            nc_from = r.nearest_cap(from_bin)
-            
-            dist_from_bin_2_nc_to = abs(from_bin.lat - nc_to.lat)   + \
-                                    abs(from_bin.long - nc_to.long)
-            dist_to_bin_2_nc_from = abs(to_bin.lat   - nc_from.lat) + \
-                                    abs(to_bin.long   - nc_from.long)
-                                    
-            if dist_from_bin_2_nc_to > dist_to_bin_2_nc_from:
-                nc_min = nc_from 
-            else:
-                nc_min = nc_to
-            lat_distance = abs(from_bin.lat -  nc_min.lat)  + abs(to_bin.lat -  nc_min.lat)
-            lng_distance = abs(from_bin.long - nc_min.long) + abs(to_bin.long - nc_min.long)
-
-        return lat_distance + lng_distance
-        
-
- 
+         
     def loc_to_loc_distance(self, from_loc, to_loc):
         '''Manhattan distance, not Euclidian
         call with two Bin class instances
@@ -131,7 +130,7 @@ class PickRoute:
                 'both arguments must be instance of Bin class'
         from_bin = Bin.get_bin_by_location(from_loc)
         to_bin   = Bin.get_bin_by_location(to_loc)
-        return self.bin_to_bin_distance(from_bin, to_bin) 
+        return bin_to_bin_distance(*sorted((from_bin, to_bin)) )
 
         
     def __repr__(self):
